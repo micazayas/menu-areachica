dcdea8bee623ba5c087f61a5b4b5b3038e6f3fe1
micazayas <micaelazayas89@gmail.com>
2025-04-23 12:39:08 +0200

    fix responsive menu

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("menu-btn").addEventListener("click", function () {
   window.location.href = "menu.html";
 });
+
+const img = document.getElementById("menuImg");
+img.src = "MENU.png?v=" + new Date().getTime();
